adbef019b8a3ce7e0101001bf114ae9b922023e7
Sergey Nazhestkin <9497020@mail.ru>
2022-11-12 23:37:15 +0300

    Add project files.

---
+++ Notes.WebApi/Controllers/BaseController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Security.Claims;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Notes.WebApi.Controllers
+{
+    public abstract class BaseController : ControllerBase
+    {
+        private IMediator _mediator;
+        protected IMediator Mediator => _mediator ??= HttpContext.RequestServices.GetService<IMediator>();
+
+        internal Guid UserId => !User.Identity.IsAuthenticated
+            ? Guid.Empty
+            : Guid.Parse(User.FindFirst(ClaimTypes.NameIdentifier).Value);
+    }
+}
---
+++ Notes.WebApi/Controllers/NoteController.cs
@@ -0,0 +1,20 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Notes.Application.Notes.Queries.GetNoteList;
+
+namespace Notes.WebApi.Controllers
+{
+    public class NoteController : BaseController
+    {
+        [HttpGet]
+        public async Task<ActionResult<NoteListVm>> GetAll()
+        {
+            var query = new GetNoteListQuery
+            {
+                UserId = UserId
+            };
+            var vm = await Mediator.Send(query);
+            return Ok(vm);
+        }
+    }
+}
